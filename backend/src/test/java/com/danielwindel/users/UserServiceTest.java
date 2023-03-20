64552df103c07b7cee6d38df3834cc40ec3a8c69
daniel <daniel.windel@unitybox.de>
2023-03-20 15:55:12 +0100

    Add exception test for addUser in UserService

--- backend/src/test/java/com/danielwindel/users/UserServiceTest.java
+++ backend/src/test/java/com/danielwindel/users/UserServiceTest.java
@@ -1,8 +1,12 @@
 package com.danielwindel.users;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -14,10 +18,12 @@ class UserServiceTest {
     UserService userService = new UserService(userRepository, userIdService);
 
     UserDTO testUserDTO = new UserDTO("test_email_address", "test_password", "Traveler");
+
     User testUser = new User("1", "test_email_address", "test_password", "Traveler");
 
     @Test
     void isAddUserAddingUser() {
+
         when(userRepository.save(testUser)).
                 thenReturn(testUser);
 
@@ -27,5 +33,15 @@ class UserServiceTest {
         User actual = userService.addUser(testUserDTO);
 
         assertEquals(testUser, actual);
+    }
+
+    @Test
+    void isExceptionThrownWhenUserCannotBeAddedToMongoDB() {
+        when(userRepository.save(any(User.class))).
+                thenThrow(new ResponseStatusException(HttpStatus.CONFLICT));
+
+        assertThrows(ResponseStatusException.class, () -> {
+            userService.addUser(testUserDTO);
+        });
     }
 }
