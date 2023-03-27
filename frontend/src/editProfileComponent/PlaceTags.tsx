1f2618d8148d3014f75391816efccb986cd13ae7
daniel <daniel.windel@unitybox.de>
2023-03-27 14:54:07 +0200

    Add autocomplete places feature in frontend

---
+++ frontend/src/editProfileComponent/PlaceTags.tsx
@@ -0,0 +1,59 @@
+import * as React from 'react';
+import {ChangeEvent, useState} from 'react';
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+import axios from "axios";
+import {Place} from "./Place";
+
+export default function PlaceTags() {
+
+
+    const [inputValue, setInputValue] = useState<string>("");
+    const [options, setOptions] = useState<string[]>([]);
+    const [place, setPlace] = useState<Place>({name: ""})
+
+
+    const handleInputChange = async (event: ChangeEvent<HTMLInputElement>) => {
+        const query = event.target.value;
+        setInputValue(query);
+        setPlace({
+            name: query
+        })
+
+        if (query) {
+            const res = await axios.post("/api/places/search", place);
+            const cities = res.data;
+            setOptions(cities);
+        } else {
+            setOptions([]);
+        }
+    };
+
+    return (
+        <div>
+            <Autocomplete
+                multiple
+                autoComplete
+                id="tags-standard"
+                limitTags={5}
+                options={options}
+                getOptionLabel={(option) => option}
+                defaultValue={[]}
+                inputValue={inputValue}
+                onInputChange={(event, inputValue, reason) => {
+                    if (reason === "reset") {
+                        setInputValue("");
+                    }
+                }}
+                renderInput={(params) => (
+                    <TextField
+                        {...params}
+                        label="Add up to 5 cities"
+                        placeholder="Cities"
+                        onChange={handleInputChange}
+                    />
+                )}
+            />
+        </div>
+    )
+}
