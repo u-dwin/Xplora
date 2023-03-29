dfc72159b451ea866ae9f57970f879a4dfc1a171
daniel <daniel.windel@unitybox.de>
2023-03-29 10:30:13 +0200

    Add MockBean to UserControllerTest

--- backend/src/test/java/com/danielwindel/places/PlaceSearchControllerTest.java
+++ backend/src/test/java/com/danielwindel/places/PlaceSearchControllerTest.java
@@ -1,10 +1,12 @@
 package com.danielwindel.places;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.maps.GeoApiContext;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -17,6 +19,9 @@ class PlaceSearchControllerTest {
 
     @Autowired
     MockMvc mockMvc;
+
+    @MockBean
+    GeoApiContext geoApiContext;
     ObjectMapper mapper = new ObjectMapper();
     PlaceDTO placeDTO = new PlaceDTO("TestPlace");
 
