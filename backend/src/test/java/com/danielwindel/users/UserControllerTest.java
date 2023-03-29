d209af3fa078d25d1ad9def82817916fb16faffc
daniel <daniel.windel@unitybox.de>
2023-03-29 09:53:24 +0200

    Add MockBean to UserControllerTest

--- backend/src/test/java/com/danielwindel/users/UserControllerTest.java
+++ backend/src/test/java/com/danielwindel/users/UserControllerTest.java
@@ -1,10 +1,12 @@
 package com.danielwindel.users;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.maps.GeoApiContext;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
@@ -24,6 +26,9 @@ class UserControllerTest {
 
     @Autowired
     private UserDetailsRepository userDetailsRepository;
+
+    @MockBean
+    private GeoApiContext geoApiContext;
 
     ObjectMapper mapper = new ObjectMapper();
 
