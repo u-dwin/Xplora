f4e5cb4a709db0ab05989581cec7bce28c549d76
daniel <daniel.windel@unitybox.de>
2023-04-04 15:24:56 +0200

    Add photo controller

---
+++ backend/src/main/java/com/danielwindel/users/PhotoController.java
@@ -0,0 +1,24 @@
+package com.danielwindel.users;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("api/photos")
+public class PhotoController {
+    private final PhotoService photoService;
+
+    PhotoController(PhotoService photoService) {
+        this.photoService = photoService;
+    }
+
+    @PostMapping("/add")
+    public String uploadPhoto(MultipartFile photo) throws IOException {
+        return photoService.uploadPhoto(photo);
+    }
+
+}
