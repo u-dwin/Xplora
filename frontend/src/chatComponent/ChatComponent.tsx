8cce540462e85b2c38832e25b8b5518c3fa6cd91
daniel <daniel.windel@unitybox.de>
2023-04-17 13:21:13 +0200

    Bugfix message userId not set correctly, add conditional rendering

--- frontend/src/chatComponent/ChatComponent.tsx
+++ frontend/src/chatComponent/ChatComponent.tsx
@@ -5,6 +5,8 @@ import Cookies from "js-cookie";
 import {ChangeEvent, useState} from "react";
 import {MessageType} from "./MessageType";
 import MessageComponent from "./MessageComponent";
+import SendIcon from '@mui/icons-material/Send';
+
 
 export default function ChatComponent() {
     const {id} = useParams()
@@ -22,7 +24,7 @@ export default function ChatComponent() {
             const messageData = JSON.parse(event.data)
             const time = messageData.time.substring(11, 16)
             const text = messageData.text
-            const userId = messageData.id
+            const userId = messageData.userId
             const newMessage: MessageType = {time, text, userId};
             setReceivedInstantMessages((prevMessages) => [...prevMessages, newMessage])
         }
@@ -39,39 +41,95 @@ export default function ChatComponent() {
         }
     };
 
-
-
     return (
         <Box sx={{
             display: "flex",
             flexDirection: "column",
             flexWrap: "wrap",
-            rowGap: "20px",
+            rowGap: "5px",
+            width: "100%",
+            alignItems: "center",
+            alignContent: "center",
             justifyContent: "center",
-            width: "35ch",
-            alignItems: "center"
+            justifyItems: "center",
+            height: "100%"
         }}>
             <Box sx={{
                 display: "flex",
-                flexDirection: "row",
+                flexDirection: "column",
                 flexWrap: "wrap",
-                rowGap: "5px"
+                justifyContent: "bottom",
+                justifyItems: "bottom",
+                alignItems: "bottom",
+                alignContent: "bottom",
+                width: "35ch",
+                height: "100%"
             }}>
+                <Box sx={{
+                    flexGrow: 1,
+                }}>
+                    {receivedInstantMessages.map((message) => (
+                        <Box
+                            sx={{
+                                display: "flex",
+                                flexDirection: "column",
+                                border: "2px",
+                                borderColor: "black",
+                                borderStyle: "solid",
+                            }}
+                            key={`${message.text}-${message.time}-${message.userId}`}
+                        >
+                            {message.userId === userId ? (
+                                <Box
+                                    sx={{
+                                        alignSelf: "flex-end",
+                                    }}
+                                >
+                                    <MessageComponent
+                                        text={message.text}
+                                        userId={message.userId}
+                                        time={message.time}
+                                    />
+                                </Box>
+                            ) : (
+                                <Box
+                                    sx={{
+                                        alignSelf: "flex-start",
+                                    }}
+                                >
+                                    <MessageComponent
+                                        text={message.text}
+                                        userId={message.userId}
+                                        time={message.time}
+                                    />
+                                </Box>
+                            )}
+                        </Box>
+                    ))}
+                </Box>
 
-                <TextField
-                    size="small"
-                    placeholder="Enter a message..."
-                    id="outlined-size-normal"
-                    value={inputMessage}
-                    onChange={handleInputChange}
-                    onKeyDown={handleKeyDown}
-                />
+                <Box
+                    sx={{
+                        display: "flex",
+                        justifyContent: "center",
+                        flexDirection: "row",
+                        alignItems: "center",
+                    }}>
 
-                {receivedInstantMessages.map((message) => (
-                        <MessageComponent text={message.text} userId={message.userId} time={message.time}/>
-                    )
-                )}
+                    <Box sx={{width: "5px"}}/>
 
+                    <TextField
+                        sx={{width: "30ch"}}
+                        size="small"
+                        placeholder="Enter a message..."
+                        id="outlined-size-normal"
+                        value={inputMessage}
+                        onChange={handleInputChange}
+                        onKeyDown={handleKeyDown}
+                    />
+                    <SendIcon sx={{width: "25px", height: "25px"}}/>
+                </Box>
+                <Box sx={{width: "5px"}}/>
             </Box>
         </Box>
     )
--- frontend/src/chatComponent/MessageComponent.tsx
+++ frontend/src/chatComponent/MessageComponent.tsx
@@ -4,18 +4,8 @@ import {MessageType} from "./MessageType";
 export default function MessageComponent(props: MessageType) {
 
     return (
-        <Box sx={{
-            display: "flex",
-            flexDirection: "column",
-            flexWrap: "wrap",
-            rowGap: "20px",
-            justifyContent: "center",
-            width: "25ch",
-            alignItems: "center"
-        }}>
             <Box>
                 {props.text} {props.time}
             </Box>
-        </Box>
     )
 }
