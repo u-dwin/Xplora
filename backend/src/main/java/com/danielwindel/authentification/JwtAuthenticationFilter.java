5ccd32c7c565988ddd9ef8a7fc3d850e390ec0ca
Daniel <daniel@danielwindel.com>
2023-09-27 16:21:22 +0200

    Embed JtwService token verification in JwtAuthenticationFilter for user authentication

--- backend/src/main/java/com/danielwindel/authentification/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/danielwindel/authentification/JwtAuthenticationFilter.java
@@ -1,11 +1,16 @@
 package com.danielwindel.authentification;
 
+import com.danielwindel.users.MongoUserDetailService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -16,6 +21,7 @@ import java.io.IOException;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
+    private final MongoUserDetailService mongoUserDetailService;
 
     @Override
     protected void doFilterInternal(
@@ -34,5 +40,23 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         }
         jwtToken = authHeader.substring(7);
         userEmail = jwtService.extractUsername(jwtToken);
+
+        if(userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null){
+            UserDetails userDetails = this.mongoUserDetailService.loadUserByUsername(userEmail);
+
+            if (jwtService.isTokenValid(jwtToken, userDetails)) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                );
+                authToken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request)
+                );
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+            }
+        filterChain.doFilter(request, response);
+        }
     }
-}
+
