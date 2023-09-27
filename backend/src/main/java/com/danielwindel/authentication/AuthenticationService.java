d12c1d510e5043be4ea022384c629a3d90fbc61f
Daniel <daniel@danielwindel.com>
2023-09-27 18:06:13 +0200

    Add logic for user authentication after registration

--- backend/src/main/java/com/danielwindel/authentication/AuthenticationService.java
+++ backend/src/main/java/com/danielwindel/authentication/AuthenticationService.java
@@ -1,4 +1,27 @@
 package com.danielwindel.authentication;
 
+import com.danielwindel.users.MongoUserDetailService;
+import com.danielwindel.users.UserDTO;
+import com.danielwindel.users.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class AuthenticationService {
+
+    private final UserService userService;
+    private final JwtService jwtService;
+    private final MongoUserDetailService mongoUserDetailService;
+
+    public AuthenticationResponse register(UserDTO userDTO) {
+       userService.addUser(userDTO);
+       var jwtToken = jwtService.generateToken(mongoUserDetailService.loadUserByUsername(userDTO.getUserEmailAddress()));
+       return AuthenticationResponse.builder()
+               .token(jwtToken)
+               .build();
+    }
+
+    public AuthenticationResponse authenticate(UserAuthDTO userAuthDTO) {
+    }
 }
